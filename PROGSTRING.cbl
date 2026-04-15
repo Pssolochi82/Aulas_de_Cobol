ba479c2a7d4f458fcdbe6e7209f72cd5e12489d3
PALMIRA JOSÉ DA SILVA SOLOCHI <psolochi2016@gmail.com>
2026-04-15 18:03:21 +0100

    novo exercicio cobol

--- PROGSTRING.cbl
+++ PROGSTRING.cbl
@@ -10,7 +10,12 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        77 WS-CONTEUDO                 PIC X(30) VALUE SPACES.
+       77 WS-TEXTO                    PIC X(40) VALUE SPACES.
+       77 WS-PONTEIRO                 PIC 9(02) VALUE ZEROS.
        PROCEDURE DIVISION.
+      *****************FORMA1*******************************************
+            INITIALISE WS-CONTEUDO
+                       WS-TEXTO
 
             STRING
                  'PALMIRA'
@@ -20,6 +25,46 @@
             END-STRING
 
             DISPLAY WS-CONTEUDO
+
+      ********************FORMA 2***************************************
+       INITIALISE WS-CONTEUDO
+                  WS-TEXTO(1:7)
+                  WS-TEXTO(15:4)
+            MOVE 'A PALMIRA ESTA A ESTUDAR COBOL' TO WS-TEXTO
+
+            STRING
+                WS-TEXTO
+                DELIMITED BY SIZE INTO WS-CONTEUDO
+            END-STRING
+
+            DISPLAY WS-CONTEUDO
+      **************************FORMA 3 ********************************
+       INITIALISE WS-CONTEUDO
+                  WS-TEXTO
+
+            MOVE 'A PALMIRA ESTA A ESTUDAR COBOL' TO WS-TEXTO
+            STRING
+                WS-TEXTO
+                DELIMITED BY SPACES INTO WS-CONTEUDO
+            END-STRING
+
+            DISPLAY WS-CONTEUDO
+
+      **************************FORMA 4 ********************************
+       INITIALISE WS-CONTEUDO
+                  WS-TEXTO
+
+            MOVE 'A        ESTA A ESTUDAR COBOL' TO WS-TEXTO
+            SET WS-PONTEIRO                      TO 4
+
+            STRING
+                'PALMIRA'
+                DELIMITED BY SIZE INTO WS-TEXTO
+                WITH POINTER WS-PONTEIRO
+            END-STRING
+
+            DISPLAY WS-TEXTO
+            DISPLAY WS-PONTEIRO
 
             STOP RUN.
        END PROGRAM PROGSTRING.
